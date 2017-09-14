76df681814f76306667346d68ddd88aebfce33b8
Lee Pender <lpender@gmail.com>
2017-09-14 09:52:54 -0400

    Add OG tags to the header

--- server/server-side-rendering.js
+++ server/server-side-rendering.js
@@ -10,10 +10,17 @@ onPageLoad(sink => {
     const backgroundUrl = mix && mix.backgroundUrl || "/space-sun-solar-flare-animation-8.gif";
 
     sink.appendToHead(`<title>${slug}</title>`);
+    sink.appendToHead(`<meta property="og:type" content="article" />`);
+    sink.appendToHead(`<meta property="og:url" content="http://www.litmix.es/${slug}" />`);
     sink.appendToHead(`<meta property="og:image" content="${backgroundUrl}" />`);
+    sink.appendToHead(`<meta property="og:title" content="${slug}" />`);
     sink.appendToHead(`<link rel="icon" href="${backgroundUrl}"/>`);
   } else {
     sink.appendToHead(`<title>LitMixes</title>`);
+    sink.appendToHead(`<meta property="og:type" content="website" />`);
+    sink.appendToHead(`<meta property="og:url" content="http://www.litmix.es/" />`);
+    sink.appendToHead(`<meta property="og:image" content="/space-sun-solar-flare-animation-8.gif" />`);
+    sink.appendToHead(`<meta property="og:title" content="LitMixes" />`);
     sink.appendToHead(`<link rel="icon" href="/space-sun-solar-flare-animation-8.gif"/>`);
   }
 })
