cfffefeba5e1c05d47e43da23ae71d55cbf490f0
Lee Pender <lpender@gmail.com>
2018-12-25 10:23:35 -0500

    Increase to 5 seconds

--- client/js/youtube.js
+++ client/js/youtube.js
@@ -1,68 +1,67 @@
 export default class YoutubeHelper {
   constructor() {
-    this.isReady = new ReactiveVar()
-    this.videoId = new ReactiveVar()
+    this.isReady = new ReactiveVar();
+    this.videoId = new ReactiveVar();
   }
 
   setVideoId(videoId) {
-    this.videoId.set(videoId)
+    this.videoId.set(videoId);
   }
 
   initialize() {
     var _this = this;
     var tag = document.createElement('script');
 
-    tag.src = "https://www.youtube.com/iframe_api";
+    tag.src = 'https://www.youtube.com/iframe_api';
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
     var player;
 
     var playerVars = {
-      autoplay: 1,        // Auto-play the video on load
-      controls: 0,        // Hide pause/play buttons in player
-      showinfo: 0,        // Hide the video title
-      modestbranding: 1,  // Hide the Youtube Logo
+      autoplay: 1, // Auto-play the video on load
+      controls: 0, // Hide pause/play buttons in player
+      showinfo: 0, // Hide the video title
+      modestbranding: 1, // Hide the Youtube Logo
       playsinline: 1,
-      loop: 1,            // Run the video in a loop
-      fs: 0,              // Hide the full screen button
-      cc_load_policy: 0,  // Hide closed captions
-      iv_load_policy: 3,  // Hide the Video Annotations
-      autohide: 0,        // Hide video controls when playing
+      loop: 1, // Run the video in a loop
+      fs: 0, // Hide the full screen button
+      cc_load_policy: 0, // Hide closed captions
+      iv_load_policy: 3, // Hide the Video Annotations
+      autohide: 0, // Hide video controls when playing
       start: 0,
       events: {
-        "onReady": function(event) {
-          let player = _this.player = event.target
+        onReady: function(event) {
+          let player = (_this.player = event.target);
 
           Tracker.autorun(function() {
-            let videoId = _this.videoId.get()
+            let videoId = _this.videoId.get();
 
             if (videoId) {
-              player.loadVideoById(videoId)
-              player.mute()
-              player.playVideo()
+              player.loadVideoById(videoId);
+              player.mute();
+              player.playVideo();
             }
           });
         },
-        "onStateChange": function(event) {
+        onStateChange: function(event) {
           if (event.data === YT.PlayerState.ENDED) {
-            let videoId = _this.videoId.get()
+            let videoId = _this.videoId.get();
 
             if (videoId) {
-              _this.player.loadVideoById(videoId)
-              _this.player.mute()
-              _this.player.playVideo()
+              _this.player.loadVideoById(videoId);
+              _this.player.mute();
+              _this.player.playVideo();
             }
           }
-        }
-      }
-
-    }
+        },
+      },
+    };
 
     window.onYouTubeIframeAPIReady = function onYouTubeIframeAPIReady() {
-      setTimeout( () => {
+      setTimeout(() => {
         new YT.Player('player', playerVars);
-      }, 1000)
-    }
+      }, 5000);
+    };
   }
 }
