26eaa8b666b5461fb2b6858035ca211aaf2f5a20
Lee Pender <lpender@gmail.com>
2019-06-11 10:25:21 -0400

    Keyboard gestures

--- client/js/keyboard.js
+++ client/js/keyboard.js
@@ -1,16 +1,16 @@
-Meteor.startup( () => {
-  window.addEventListener('keydown', (event)=> {
-    if(event.target.tagName === "INPUT")
-      return;
+Meteor.startup(() => {
+  window.addEventListener('keydown', event => {
+    if (event.target.tagName === 'INPUT') return;
 
-    if(event.code == "Space") {
+    if (event.code == 'Space') {
       playlist.playToggle();
-    } else if(event.code == "ArrowRight") {
+      event.preventDefault();
+    } else if (event.code == 'ArrowRight') {
       playlist.advance();
-    } else if(event.code == "ArrowLeft") {
+      event.preventDefault();
+    } else if (event.code == 'ArrowLeft') {
       playlist.retreat();
+      event.preventDefault();
     }
-
-    event.preventDefault();
   });
-})
+});
